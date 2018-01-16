31cc02ef50e817c144da1d4992f2adf000f1eaf5
ryo.takahashi <ryo.takahashi.work@gmail.com>
2018-01-16 20:57:29 +0900

    60. IBOutletやIBActionを設定しよう

--- Swift4SunriseApp/ViewController.swift
+++ Swift4SunriseApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var _cityNameInput: UITextField!
+    
+    @IBOutlet weak var _sunriseTimeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func findSunrise(_ sender: Any) {
+        
+    }
+    
 }
 
