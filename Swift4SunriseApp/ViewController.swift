a7cc81e63fe4753011794c7d7b486d8bb77e7cfc
ryo.takahashi <ryo.takahashi.work@gmail.com>
2018-01-16 20:45:41 +0900

    Initial Commit

---
+++ Swift4SunriseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4SunriseApp
+//
+//  Created by ryo.takahashi on 2018/01/16.
+//  Copyright © 2018年 ryo.takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
